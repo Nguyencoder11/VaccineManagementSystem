6796c40b7c9fe2002dad7eff3c9347b23348f5dd
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2025-03-23 09:32:53 +0700

    :tada: init project

---
+++ vaxms-server/src/main/java/com/app/vaxms_server/VaxmsServerApplication.java
@@ -0,0 +1,13 @@
+package com.app.vaxms_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaxmsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaxmsServerApplication.class, args);
+	}
+
+}
---
+++ vaxms-server/src/test/java/com/app/vaxms_server/VaxmsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.vaxms_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaxmsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
